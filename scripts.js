1806726ff7eaf325f6ea7d66a9731a7409ed34b7
Althea Vestrit <137403706+AltheaVestrit@users.noreply.github.com>
2025-07-08 22:42:41 +0200

    add basic functionality for 'add new book' form

--- scripts.js
+++ scripts.js
@@ -1,5 +1,11 @@
 const booksContainer = document.querySelector("#books-container");
-const newBookForm = document.querySelector("dialog");
+const dialog = document.querySelector("dialog");
+const showDialog = document.querySelector("#add-new-btn");
+const cancelBtn = document.querySelector("#cancel-btn");
+const submitBtn = document.querySelector("#submit-btn");
+const nameField = document.querySelector("#new-title");
+const authorField = document.querySelector("#new-author");
+const yearField = document.querySelector("#new-year");
 
 const myLibrary = [];
 
@@ -25,42 +31,60 @@ addBookToLibrary("Mansfield Park", "Jane Austen", 1814);
 addBookToLibrary("Persuasion", "Jane Austen", 1818);
 addBookToLibrary("Lady Susan", "Jane Austen", 1871);
 
-myLibrary.forEach((book) => {
-    const newBook = document.createElement("div");
-    newBook.classList += "book";
-    newBook.id = book.id;
+function loadLibrary() {
+    myLibrary.forEach((book) => {
+        const newBook = document.createElement("div");
+        newBook.classList += "book";
+        newBook.id = book.id;
 
-    const titleDiv = document.createElement("div");
-    titleDiv.classList += "title";
+        const titleDiv = document.createElement("div");
+        titleDiv.classList += "title";
 
-    const titleText = document.createElement("h2");
-    titleText.innerText = book.name;
+        const titleText = document.createElement("h2");
+        titleText.innerText = book.name;
 
-    const detailsDiv = document.createElement("div");
-    detailsDiv.classList += "details";
+        const detailsDiv = document.createElement("div");
+        detailsDiv.classList += "details";
 
-    const authorP = document.createElement("p");
-    authorP.classList += "author";
-    authorP.innerText = book.author;
+        const authorP = document.createElement("p");
+        authorP.classList += "author";
+        authorP.innerText = book.author;
 
-    const yearP = document.createElement("p");
-    yearP.classList += "year";
-    yearP.innerText = book.year;
+        const yearP = document.createElement("p");
+        yearP.classList += "year";
+        yearP.innerText = book.year;
 
-    const idP = document.createElement("p");
-    idP.classList += "id";
-    idP.innerText = book.id;
+        const idP = document.createElement("p");
+        idP.classList += "id";
+        idP.innerText = book.id;
 
-    detailsDiv.appendChild(authorP);
-    detailsDiv.appendChild(yearP);
-    detailsDiv.appendChild(idP);
+        detailsDiv.appendChild(authorP);
+        detailsDiv.appendChild(yearP);
+        detailsDiv.appendChild(idP);
 
-    titleDiv.appendChild(titleText);
+        titleDiv.appendChild(titleText);
 
-    newBook.appendChild(titleDiv);
-    newBook.appendChild(detailsDiv);
+        newBook.appendChild(titleDiv);
+        newBook.appendChild(detailsDiv);
 
-    booksContainer.appendChild(newBook);
+        booksContainer.appendChild(newBook);
+    })
+}
+
+loadLibrary();
+
+showDialog.addEventListener("click", () => {
+    dialog.showModal();
 })
 
-newBookForm.showModal();
+cancelBtn.addEventListener("click", (e) => {
+    e.preventDefault();
+    dialog.close();
+})
+
+submitBtn.addEventListener("click", (e) => {
+    e.preventDefault();
+    addBookToLibrary(nameField.value, authorField.value, yearField.value);
+    loadLibrary();
+    dialog.close();
+})
